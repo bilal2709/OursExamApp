42c0165373fe9fa6b846d9723c83b326e6245fa8
bilal <bilal-rockstar@outlook.com>
2025-02-13 09:45:31 +0100

    initialisation

---
+++ src/main/java/ExamApp/Ours/OursApplication.java
@@ -0,0 +1,13 @@
+package ExamApp.Ours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExamApp/Ours/OursApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamApp.Ours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
